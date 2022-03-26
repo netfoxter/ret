6e6037d1919db2af1d0dd74c42a3c8acd14cbc1c
netfoxter <99637225+netfoxter@users.noreply.github.com>
2022-03-26 12:13:03 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 # ini step kedua membuat build setelah melakukan configure login aws cli pada terminal
 
-aws gamelift upload-build --name aws --build-version 1 --build-root /root/Desktop/hs/ --operating-system AMAZON_LINUX --region us-east-1
+aws gamelift upload-build --name aws --build-version 1 --build-root /root/Desktop/hs/ --operating-system AMAZON_LINUX --region us-seast-1
 aws gamelift upload-build --name aws --build-version 1 --build-root /root/Desktop/hs/ --operating-system AMAZON_LINUX --region us-east-2
 aws gamelift upload-build --name aws --build-version 1 --build-root /root/Desktop/hs/ --operating-system AMAZON_LINUX --region us-west-1
 aws gamelift upload-build --name aws --build-version 1 --build-root /root/Desktop/hs/ --operating-system AMAZON_LINUX --region us-west-2
 aws gamelift upload-build --name aws --build-version 1 --build-root /root/Desktop/hs/ --operating-system AMAZON_LINUX --region ap-south-1
+aws gamelift upload-build --name aws --build-version 1 --build-root /root/Desktop/hs/ --operating-system AMAZON_LINUX --region ap-northeast-3
 aws gamelift upload-build --name aws --build-version 1 --build-root /root/Desktop/hs/ --operating-system AMAZON_LINUX --region ap-northeast-2
 aws gamelift upload-build --name aws --build-version 1 --build-root /root/Desktop/hs/ --operating-system AMAZON_LINUX --region ap-southeast-1
 aws gamelift upload-build --name aws --build-version 1 --build-root /root/Desktop/hs/ --operating-system AMAZON_LINUX --region ap-southeast-2
@@ -13,4 +14,6 @@ aws gamelift upload-build --name aws --build-version 1 --build-root /root/Deskto
 aws gamelift upload-build --name aws --build-version 1 --build-root /root/Desktop/hs/ --operating-system AMAZON_LINUX --region eu-central-1
 aws gamelift upload-build --name aws --build-version 1 --build-root /root/Desktop/hs/ --operating-system AMAZON_LINUX --region eu-west-1
 aws gamelift upload-build --name aws --build-version 1 --build-root /root/Desktop/hs/ --operating-system AMAZON_LINUX --region eu-west-2
-
+aws gamelift upload-build --name aws --build-version 1 --build-root /root/Desktop/hs/ --operating-system AMAZON_LINUX --region eu-west-3
+aws gamelift upload-build --name aws --build-version 1 --build-root /root/Desktop/hs/ --operating-system AMAZON_LINUX --region eu-north-1
+aws gamelift upload-build --name aws --build-version 1 --build-root /root/Desktop/hs/ --operating-system AMAZON_LINUX --region sa-east-1
