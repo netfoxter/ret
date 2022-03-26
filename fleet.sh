927087045575de8c82279611aa2beae6b90e3240
netfoxter <99637225+netfoxter@users.noreply.github.com>
2022-03-26 13:18:05 +0700

    Update fleet.sh

--- fleet.sh
+++ fleet.sh
@@ -2,7 +2,7 @@
 
 aws configure set region us-east-1
 
-UILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+BUILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
 
 echo build id is $BUILD_ID
 
@@ -22,7 +22,7 @@ aws gamelift create-fleet --name aws --ec2-instance-type m4.2xlarge --build-id $
 
 aws configure set region us-east-2
 
-UILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+BUILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
 
 echo build id is $BUILD_ID
 
@@ -42,7 +42,7 @@ aws gamelift create-fleet --name aws --ec2-instance-type m4.2xlarge --build-id $
 
 aws configure set region us-west-1
 
-UILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+BUILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
 
 echo build id is $BUILD_ID
 
@@ -62,7 +62,7 @@ aws gamelift create-fleet --name aws --ec2-instance-type m4.2xlarge --build-id $
 
 aws configure set region us-west-2
 
-UILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+BUILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
 
 echo build id is $BUILD_ID
 
@@ -82,7 +82,7 @@ aws gamelift create-fleet --name aws --ec2-instance-type m4.2xlarge --build-id $
 
 aws configure set region ap-south-1
 
-UILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+BUILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
 
 echo build id is $BUILD_ID
 
@@ -102,7 +102,7 @@ aws gamelift create-fleet --name aws --ec2-instance-type m4.2xlarge --build-id $
 
 aws configure set region ap-northeast-3
 
-UILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+BUILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
 
 echo build id is $BUILD_ID
 
@@ -122,7 +122,7 @@ aws gamelift create-fleet --name aws --ec2-instance-type m4.2xlarge --build-id $
 
 aws configure set region ap-northeast-2
 
-UILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+BUILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
 
 echo build id is $BUILD_ID
 
@@ -142,7 +142,7 @@ aws gamelift create-fleet --name aws --ec2-instance-type m4.2xlarge --build-id $
 
 aws configure set region ap-southeast-1
 
-UILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+BUILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
 
 echo build id is $BUILD_ID
 
@@ -162,7 +162,7 @@ aws gamelift create-fleet --name aws --ec2-instance-type m4.2xlarge --build-id $
 
 aws configure set region ap-southeast-2
 
-UILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+BUILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
 
 echo build id is $BUILD_ID
 
@@ -182,7 +182,7 @@ aws gamelift create-fleet --name aws --ec2-instance-type m4.2xlarge --build-id $
 
 aws configure set region ap-northeast-1
 
-UILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+BUILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
 
 echo build id is $BUILD_ID
 
@@ -202,7 +202,7 @@ aws gamelift create-fleet --name aws --ec2-instance-type m4.2xlarge --build-id $
 
 aws configure set region ca-central-1
 
-UILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+BUILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
 
 echo build id is $BUILD_ID
 
@@ -222,7 +222,7 @@ aws gamelift create-fleet --name aws --ec2-instance-type m4.2xlarge --build-id $
 
 aws configure set region eu-central-1
 
-UILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+BUILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
 
 echo build id is $BUILD_ID
 
@@ -242,7 +242,7 @@ aws gamelift create-fleet --name aws --ec2-instance-type m4.2xlarge --build-id $
 
 aws configure set region eu-west-1
 
-UILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+BUILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
 
 echo build id is $BUILD_ID
 
@@ -262,7 +262,7 @@ aws gamelift create-fleet --name aws --ec2-instance-type m4.2xlarge --build-id $
 
 aws configure set region eu-west-2
 
-UILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+BUILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
 
 echo build id is $BUILD_ID
 
@@ -282,7 +282,7 @@ aws gamelift create-fleet --name aws --ec2-instance-type m4.2xlarge --build-id $
 
 aws configure set region eu-west-3
 
-UILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+BUILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
 
 echo build id is $BUILD_ID
 
@@ -302,7 +302,7 @@ aws gamelift create-fleet --name aws --ec2-instance-type m4.2xlarge --build-id $
 
 aws configure set region eu-north-1
 
-UILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+BUILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
 
 echo build id is $BUILD_ID
 
@@ -322,7 +322,7 @@ aws gamelift create-fleet --name aws --ec2-instance-type m4.2xlarge --build-id $
 
 aws configure set region sa-east-1
 
-UILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+BUILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
 
 echo build id is $BUILD_ID
 
@@ -339,6 +339,3 @@ aws gamelift create-fleet --name aws --ec2-instance-type m5a.2xlarge --build-id 
 
 
 aws gamelift create-fleet --name aws --ec2-instance-type m4.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/Server.x86_64 --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
-
-
-
