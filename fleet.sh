732765db25b365386678581add5811b60efc60c4
netfoxter <99637225+netfoxter@users.noreply.github.com>
2022-03-26 12:20:38 +0700

    Update fleet.sh

--- fleet.sh
+++ fleet.sh
@@ -1,9 +1,8 @@
 # step ketiga membuat Fleet (instance) jumlah ada 13 region
 
-
 aws configure set region us-east-1
 
-BUILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+UILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
 
 echo build id is $BUILD_ID
 
@@ -20,11 +19,10 @@ aws gamelift create-fleet --name aws --ec2-instance-type m5a.2xlarge --build-id 
 
 
 aws gamelift create-fleet --name aws --ec2-instance-type m4.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
-
 
 aws configure set region us-east-2
 
-BUILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+UILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
 
 echo build id is $BUILD_ID
 
@@ -41,10 +39,10 @@ aws gamelift create-fleet --name aws --ec2-instance-type m5a.2xlarge --build-id 
 
 
 aws gamelift create-fleet --name aws --ec2-instance-type m4.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
-
 
 aws configure set region us-west-1
-BUILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+
+UILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
 
 echo build id is $BUILD_ID
 
@@ -62,10 +60,9 @@ aws gamelift create-fleet --name aws --ec2-instance-type m5a.2xlarge --build-id 
 
 aws gamelift create-fleet --name aws --ec2-instance-type m4.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
 
-
-
 aws configure set region us-west-2
-BUILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+
+UILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
 
 echo build id is $BUILD_ID
 
@@ -84,9 +81,11 @@ aws gamelift create-fleet --name aws --ec2-instance-type m5a.2xlarge --build-id 
 aws gamelift create-fleet --name aws --ec2-instance-type m4.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
 
 aws configure set region ap-south-1
-BUILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+
+UILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
 
 echo build id is $BUILD_ID
+
 aws gamelift create-fleet --name aws --ec2-instance-type c5.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
 
 
@@ -101,10 +100,32 @@ aws gamelift create-fleet --name aws --ec2-instance-type m5a.2xlarge --build-id 
 
 aws gamelift create-fleet --name aws --ec2-instance-type m4.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
 
+aws configure set region ap-northeast-3
+
+UILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+
+echo build id is $BUILD_ID
+
+aws gamelift create-fleet --name aws --ec2-instance-type c5.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
+
+
+aws gamelift create-fleet --name aws --ec2-instance-type c5a.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
+
+
+aws gamelift create-fleet --name aws --ec2-instance-type m5.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
+
+
+aws gamelift create-fleet --name aws --ec2-instance-type m5a.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
+
+
+aws gamelift create-fleet --name aws --ec2-instance-type m4.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
 
 aws configure set region ap-northeast-2
-BUILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+
+UILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+
 echo build id is $BUILD_ID
+
 aws gamelift create-fleet --name aws --ec2-instance-type c5.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
 
 
@@ -119,12 +140,12 @@ aws gamelift create-fleet --name aws --ec2-instance-type m5a.2xlarge --build-id 
 
 aws gamelift create-fleet --name aws --ec2-instance-type m4.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
 
-
-
 aws configure set region ap-southeast-1
-BUILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+
+UILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
 
 echo build id is $BUILD_ID
+
 aws gamelift create-fleet --name aws --ec2-instance-type c5.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
 
 
@@ -140,9 +161,11 @@ aws gamelift create-fleet --name aws --ec2-instance-type m5a.2xlarge --build-id 
 aws gamelift create-fleet --name aws --ec2-instance-type m4.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
 
 aws configure set region ap-southeast-2
-BUILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+
+UILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
 
 echo build id is $BUILD_ID
+
 aws gamelift create-fleet --name aws --ec2-instance-type c5.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
 
 
@@ -158,9 +181,11 @@ aws gamelift create-fleet --name aws --ec2-instance-type m5a.2xlarge --build-id 
 aws gamelift create-fleet --name aws --ec2-instance-type m4.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
 
 aws configure set region ap-northeast-1
-BUILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+
+UILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
 
 echo build id is $BUILD_ID
+
 aws gamelift create-fleet --name aws --ec2-instance-type c5.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
 
 
@@ -176,9 +201,11 @@ aws gamelift create-fleet --name aws --ec2-instance-type m5a.2xlarge --build-id 
 aws gamelift create-fleet --name aws --ec2-instance-type m4.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
 
 aws configure set region ca-central-1
-BUILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+
+UILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
 
 echo build id is $BUILD_ID
+
 aws gamelift create-fleet --name aws --ec2-instance-type c5.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
 
 
@@ -194,9 +221,11 @@ aws gamelift create-fleet --name aws --ec2-instance-type m5a.2xlarge --build-id 
 aws gamelift create-fleet --name aws --ec2-instance-type m4.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
 
 aws configure set region eu-central-1
-BUILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+
+UILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
 
 echo build id is $BUILD_ID
+
 aws gamelift create-fleet --name aws --ec2-instance-type c5.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
 
 
@@ -212,9 +241,11 @@ aws gamelift create-fleet --name aws --ec2-instance-type m5a.2xlarge --build-id 
 aws gamelift create-fleet --name aws --ec2-instance-type m4.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
 
 aws configure set region eu-west-1
-BUILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+
+UILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
 
 echo build id is $BUILD_ID
+
 aws gamelift create-fleet --name aws --ec2-instance-type c5.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
 
 
@@ -230,9 +261,11 @@ aws gamelift create-fleet --name aws --ec2-instance-type m5a.2xlarge --build-id 
 aws gamelift create-fleet --name aws --ec2-instance-type m4.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
 
 aws configure set region eu-west-2
-BUILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+
+UILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
 
 echo build id is $BUILD_ID
+
 aws gamelift create-fleet --name aws --ec2-instance-type c5.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
 
 
@@ -246,3 +279,66 @@ aws gamelift create-fleet --name aws --ec2-instance-type m5a.2xlarge --build-id 
 
 
 aws gamelift create-fleet --name aws --ec2-instance-type m4.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
+
+aws configure set region eu-west-3
+
+UILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+
+echo build id is $BUILD_ID
+
+aws gamelift create-fleet --name aws --ec2-instance-type c5.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
+
+
+aws gamelift create-fleet --name aws --ec2-instance-type c5a.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
+
+
+aws gamelift create-fleet --name aws --ec2-instance-type m5.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
+
+
+aws gamelift create-fleet --name aws --ec2-instance-type m5a.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
+
+
+aws gamelift create-fleet --name aws --ec2-instance-type m4.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
+
+aws configure set region eu-north-1
+
+UILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+
+echo build id is $BUILD_ID
+
+aws gamelift create-fleet --name aws --ec2-instance-type c5.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
+
+
+aws gamelift create-fleet --name aws --ec2-instance-type c5a.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
+
+
+aws gamelift create-fleet --name aws --ec2-instance-type m5.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
+
+
+aws gamelift create-fleet --name aws --ec2-instance-type m5a.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
+
+
+aws gamelift create-fleet --name aws --ec2-instance-type m4.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
+
+aws configure set region sa-east-1
+
+UILD_ID=$(aws gamelift list-builds --output text --query 'Builds[0].BuildId' 2> /dev/null)
+
+echo build id is $BUILD_ID
+
+aws gamelift create-fleet --name aws --ec2-instance-type c5.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
+
+
+aws gamelift create-fleet --name aws --ec2-instance-type c5a.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
+
+
+aws gamelift create-fleet --name aws --ec2-instance-type m5.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
+
+
+aws gamelift create-fleet --name aws --ec2-instance-type m5a.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
+
+
+aws gamelift create-fleet --name aws --ec2-instance-type m4.2xlarge --build-id $BUILD_ID --server-launch-path /local/game/install.bat --server-launch-parameters run+install.bat --ec2-inbound-permissions FromPort=22,ToPort=22,IpRange=0.0.0.0/0,Protocol=TCP
+
+
+
